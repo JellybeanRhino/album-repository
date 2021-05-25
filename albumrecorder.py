264ff7156ab53d700f98ff851bdad9454b292dde
Rhi <79292775+JellybeanRhino@users.noreply.github.com>
2021-05-25 12:08:33 +1200

    Created main and menu
    Programmed is planned and commented to begin the menu.

---
+++ albumrecorder.py
@@ -0,0 +1,57 @@
+##
+# album_recommender.py
+# Rhiannon MacCreadie
+# Class code practice assessment
+# Asks user to enter an album
+# Album contains title,artist, genre, rating
+# User can add, edit, delete, and rate albums
+# If the user rates album it should recommend another one
+
+# Add Function
+
+
+# Edit Function
+
+
+# Delete Function
+
+
+# Display Function
+
+
+# Rate Function
+
+
+# Recommend Function
+
+
+# Menu
+def menu(albums):
+    """
+Displays the option to the user and calls accordingly.
+    """
+
+    # Prints the menu and loops until user quits
+    while True:
+        print("""
+Welcome to Album rater and recommender
+(A)dd a album
+(E)dit an album
+(D)elete an album
+(P)rint all albums
+(R)ate an album
+(Q)uit """)
+        choice = input("Please enter your option... ").upper()
+
+        # Calls the users options
+        if 
+              
+
+
+# Main Routine
+if __name__ == "__main__":
+    # declearing a  dictionary of  album dictionaries
+    albums = {}
+    menu(albums)
+    
+    
