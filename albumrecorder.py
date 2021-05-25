c141f15c33e405a2250d024ea69c4f0e9bd1d0bf
Rhi <79292775+JellybeanRhino@users.noreply.github.com>
2021-05-25 12:36:30 +1200

    Commit changes

--- albumrecorder.py
+++ albumrecorder.py
@@ -7,8 +7,33 @@
 # User can add, edit, delete, and rate albums
 # If the user rates album it should recommend another one
 
-# Add Function
+def add(albums):
+    """
+Adds an album, to the albums collection
+Asks user for title, artist, genre, and rating
+Returns the updated album
+    """
+    # Add title, artist, and genre which are strings
+    title = input("Title: ")
+    artist = input("Artist: ")
+    genre = input("Genre: ")
 
+    # Add a rating between 1 to 5 and forces input
+    MIN_RATING = 1
+    MAX_RATING = 5
+    while True:
+        try:
+            rating = int(input("Rating out of {}: ".format(MAX_RATING)))
+            if rating < MIN_RATING or rating > MAX_RATING:
+                print("Rating must be between {}-{}".format(MIN_RATING,MAX_RATING))
+            else:
+                break
+        except ValueError:
+            print("Rating must be between {}-{}".format(MIN_RATING,MAX_RATING))
+    # Add the vakues to the album dictionary
+    album = {"title":title, "artist":artist, "genre":genre, "rating":rating}
+
+    # Add our album to our albums collection
 
 # Edit Function
 
@@ -26,7 +51,7 @@
 
 
 # Menu
-def menu(albums):
+def menu(albums, album_id):
     """
 Displays the option to the user and calls accordingly.
     """
@@ -47,11 +72,14 @@ Welcome to Album rater and recommender
         if 
               
 
-
+return albums, album_id
 # Main Routine
 if __name__ == "__main__":
     # declearing a  dictionary of  album dictionaries
     albums = {}
-    menu(albums)
+
+    # track current album ID
+    album_id = 0
+    menu(albums, album_id)
     
     
